3f3140b990e3ee57128ac6cbc640b7b538d5a2f2
Furkan Aydin <n61furkan@gmail.com>
2024-12-19 10:26:11 +0300

    Get EntitySet read kullanımı

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,13 +1,34 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
+function (Controller, MessageToast, JSONModel) {
     "use strict";
 
     return Controller.extend("fioriodatav2.controller.View1", {
         onInit: function () {
            var oModel = this.getOwnerComponent().getModel();
            this.getView().setModel(oModel, "mPers") 
+        },
+
+        onPressReadList(){
+            var oView = this.getView();
+            var oModel = oView.getModel();
+            
+            var sPath = "/persProcessSet";
+            oModel.read(sPath, {
+                success: function(oSuccess){
+                var oJSONModel = new JSONModel();
+                oJSONModel.setData(oSuccess);
+                oView.setModel(oJSONModel, "mPers");
+                MessageToast.show("Success");
+                },
+                error: function(oError){
+                    var sMessage = JSON.parse(oError.responseText).error.message.value;
+                    MessageToast.show("Error:" + sMessage);
+                }
+            });
         }
     });
 });
